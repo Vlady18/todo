fcc6983661d6bbd5f6f9af8663980f12047cdb72
Mazharenko <mazhor02@gmail.com>
2020-02-04 22:34:23 +0200

    redux-todod

--- src/context/FirebaseState.js
+++ src/context/FirebaseState.js
@@ -1,14 +1,19 @@
 import React from "react";
 import {FirebaseContext} from "./firebaseContext";
+import axios from 'axios'
 
 
 export const FirebaseState = ({children})=>{
     const addNote = async ()=>{
-        const res = await axios.post()
+        const res = await axios.post();
+    }
+    const fetchNote = async ()=>{
+        const res = await axios.get()
     }
     return(
         <FirebaseContext.Provider
-
+            addNote={addNote}
+            fetchNote={fetchNote}
         >
             {children}
         </FirebaseContext.Provider>
